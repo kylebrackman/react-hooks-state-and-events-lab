8426fe5e8cb403225a66d20cf84def303c5219bf
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-11-05 21:19:50 -0600

    commit tree e0818ade936decc154581acac18922353959edde

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, setCart] = useState(false)
+  function addToCart (event) {
+    setCart(() => !inCart)
+    console.log(event.target.parentElement.className)
+  }
   return (
-    <li className="">
+    <li className= {inCart ? 'in-cart' : " "}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick = { addToCart }> {inCart ? "Remove from cart" : "Add to cart"} </button>
     </li>
   );
 }
